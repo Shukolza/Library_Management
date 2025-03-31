da6fbc28c66bcc8731cd5b6bbd99db2929416908
shukolza <shukolza@gmail.com>
2025-03-31 17:03:26 +0600

    docs: Update README & add LICENSE

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ from pathlib import Path
 
 # CHANGE THIS TO USE CUSTOM DB / ИЗМЕНИТЕ ЭТО ДЛЯ СВОЕЙ БД
 DB_PATH: Path = resource_path("./libs_data.json")
+
 # CHANGE THIS TO USE CUSTOM ICON / ИЗМЕНИТЕ ЭТО ДЛЯ СВОЕЙ ИКОНКИ
 ICON_PATH: Path = resource_path("./img/book.png")
